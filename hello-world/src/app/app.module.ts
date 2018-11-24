df5ec42b55781fedd84d9ec73d8181f1d8813045
smpower <rf.wangchn@gmail.com>
2018-11-24 23:26:20 +0800

    add component: user-item

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
