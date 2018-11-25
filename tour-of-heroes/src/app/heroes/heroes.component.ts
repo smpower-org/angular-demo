3487c5546975f7b6c6521e22b901aaa3a7000a86
smpower <rf.wangchn@gmail.com>
2018-11-25 14:59:09 +0800

    显示英雄列表

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({  // <-- 装饰器, 用于为该组件指定 Angular 所需的元数据
   selector: 'app-heroes',  // <-- 组件的选择器
@@ -12,8 +13,16 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
+  console.log(HEROES);
   ngOnInit() {  // <-- 生命周期钩子, 这里放置初始化逻辑
   }
 
