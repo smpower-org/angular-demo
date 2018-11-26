8c6c32199e638c8c05bf9de57587146c08b7d024
smpower <rf.wangchn@gmail.com>
2018-11-26 15:02:50 +0800

    服务

---
+++ tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';  // <-- 导入 Injectable 符号
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({  // <-- 添加 @Injectable 装饰器
+  providedIn: 'root'  // <-- 接受该元素的元数据对象
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_  fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({  // <-- 装饰器, 用于为该组件指定 Angular 所需的元数据
   selector: 'app-heroes',  // <-- 组件的选择器
@@ -8,22 +9,21 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']  // <-- 组件私有 CSS 样式表文件的位置
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  heroes = HEROES;
-
+  // heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {  // <-- 生命周期钩子, 这里放置初始化逻辑
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  console.log(HEROES);
-  ngOnInit() {  // <-- 生命周期钩子, 这里放置初始化逻辑
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
 }
